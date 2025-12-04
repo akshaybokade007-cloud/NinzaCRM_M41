3e2641418510aff5bde0f7a1e470e467c79cfc20
akshaybokade007-cloud <akshaybokade007@gmail.com>
2025-12-04 10:39:02 +0530

    Committed

---
+++ src/test/java/Demo1/DemoTest.java
@@ -0,0 +1,12 @@
+package Demo1;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test(groups = "Regression")
+	public void dhoni() {
+		System.out.println("Dhoni");
+	}
+	
+}
---
+++ src/test/java/Demo1/SampleTest.java
@@ -0,0 +1,12 @@
+package Demo1;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	
+	@Test(groups = "Smoke")
+	public void virat() {
+		System.out.println("Virat");
+	}
+}
